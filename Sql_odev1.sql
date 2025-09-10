26138301cf077c8a4c8c7a58a5d5355265f93900
Zeynep <138984822+zeyneptekkk@users.noreply.github.com>
2025-09-10 21:11:01 +0300

    Update Sql_odev1.sql

--- Sql_odev1.sql
+++ Sql_odev1.sql
@@ -1,9 +1,9 @@
 create table books(
 book_id int identity(1,1) primary key,
-title varchar(50) not null,
-author varchar(50) not null,
-genre varchar(50) ,
-price float,
+title nvarchar(50) not null,
+author nvarchar(50) not null,
+genre nvarchar(50) ,
+price decimal(10,2) check(price>0),
 stock_qt int,
 published_year varchar(20),
 added_at date,
@@ -11,37 +11,37 @@ added_at date,
 
 
 insert into books(title,author,genre,price,stock_qt,published_year,added_at)
-values('Kayıp Zamanın İzninde','M. Proust','roman',129.90,25,1913,'2025-08-20'),
-('Simyacı','P.Coelho','roman',89.50,40,1988,'2025-08-21'),
+values('KayÃ½p ZamanÃ½n Ãzninde','M. Proust','roman',129.90,25,1913,'2025-08-20'),
+('SimyacÃ½','P.Coelho','roman',89.50,40,1988,'2025-08-21'),
 ('Sapiens','Y.N Harari','tarih',159.00,18,2011,'2025-08-25'),
-('Simyacı','P.Coelho','romqn',89.50,40,1988,'2025-08-21'),
-('İnce Memed', 'Y. Kemal', 'roman', 99.90, 12, 1955, '2025-08-22'),
-('Körlük', 'J. Saramago', 'roman', 119.00, 7, 1995, '2025-08-28'),
+('SimyacÃ½','P.Coelho','romqn',89.50,40,1988,'2025-08-21'),
+('Ãnce Memed', 'Y. Kemal', 'roman', 99.90, 12, 1955, '2025-08-22'),
+('KÃ¶rlÃ¼k', 'J. Saramago', 'roman', 119.00, 7, 1995, '2025-08-28'),
 ('Dune', 'F. Herbert', 'bilim', 149.00, 30, 1965, '2025-09-01'),
-('Hayvan Çiftliği', 'G. Orwell', 'roman', 79.90, 55, 1945, '2025-08-23'),
+('Hayvan Ã‡iftliÃ°i', 'G. Orwell', 'roman', 79.90, 55, 1945, '2025-08-23'),
 ('1984', 'G. Orwell', 'roman', 99.00, 35, 1949, '2025-08-24'),
-('Nutuk', 'M. K. Atatürk', 'tarih', 139.00, 20, 1927, '2025-08-27'),
-('Küçük Prens', 'A. de Saint-Exupéry', 'çocuk', 69.90, 80, 1943, '2025-08-26'),
-('Başlangıç', 'D. Brown', 'roman', 109.00, 22, 2017, '2025-09-02'),
-('Atomik Alışkanlıklar', 'J. Clear', 'kişisel gelişim', 129.00, 28, 2018, '2025-09-03'),
-('Zamanın Kısa Tarihi', 'S. Hawking', 'bilim', 119.50, 16, 1988, '2025-08-29'),
-('Şeker Portakalı', 'J. M. de Vasconcelos', 'roman', 84.90, 45, 1968, '2025-08-30'),
-('Bir İdam Mahkûmunun Son Günü', 'V. Hugo', 'roman', 74.90, 26, 1829, '2025-08-31')
+('Nutuk', 'M. K. AtatÃ¼rk', 'tarih', 139.00, 20, 1927, '2025-08-27'),
+('KÃ¼Ã§Ã¼k Prens', 'A. de Saint-ExupÃ©ry', 'Ã§ocuk', 69.90, 80, 1943, '2025-08-26'),
+('BaÃ¾langÃ½Ã§', 'D. Brown', 'roman', 109.00, 22, 2017, '2025-09-02'),
+('Atomik AlÃ½Ã¾kanlÃ½klar', 'J. Clear', 'kiÃ¾isel geliÃ¾im', 129.00, 28, 2018, '2025-09-03'),
+('ZamanÃ½n KÃ½sa Tarihi', 'S. Hawking', 'bilim', 119.50, 16, 1988, '2025-08-29'),
+('Ãeker PortakalÃ½', 'J. M. de Vasconcelos', 'roman', 84.90, 45, 1968, '2025-08-30'),
+('Bir Ãdam MahkÃ»munun Son GÃ¼nÃ¼', 'V. Hugo', 'roman', 74.90, 26, 1829, '2025-08-31')
 
 select*from books
 
 delete from books where book_id=1
 
----fiyatlarının artan sırada sıralma---
+---fiyatlarÃ½nÃ½n artan sÃ½rada sÃ½ralma---
 select title,author,price
 from books
 order by price asc
----fiyatları azalan sırada sıralama---
+---fiyatlarÃ½ azalan sÃ½rada sÃ½ralama---
 select title,author,price
 from books
 order by price desc
 
----titlelerine göre sıralama--
+---titlelerine gÃ¶re sÃ½ralama--
 select title 
 from books
 where genre='roman'
@@ -54,7 +54,7 @@ where genre='tarih'
 order by title asc
 
 
----between kukanımı---
+---between kukanÃ½mÃ½---
 
 select title,price
 from books
@@ -66,14 +66,14 @@ select title , stock_qt
 from books
 where stock_qt<20
 
----title içinde zaman gecenleri filtrele--
+---title iÃ§inde zaman gecenleri filtrele--
 
 select title,author
 from books
 where lower(title) like '%zaman%' 
 
 
----IN kullanımı---
+---IN kullanÃ½mÃ½---
 
 select title,genre
 from books
@@ -86,13 +86,13 @@ from books
 where published_year>2000 
 order by published_year desc
 
------son 10 gün içiinde eklenen kitapla---
+-----son 10 gÃ¼n iÃ§iinde eklenen kitapla---
 
 select  title ,added_at
 from books
 where added_at >=DATEADD(Day,-10,getdate())
 
------top kulanımı----
+-----top kulanÃ½mÃ½----
 
 select top 5 title ,price
 from books
