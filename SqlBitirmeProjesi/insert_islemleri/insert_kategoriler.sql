1bb2add33005828f47582b51a4257b4c0e98157c
Zeynep <138984822+zeyneptekkk@users.noreply.github.com>
2025-09-30 00:06:27 +0300

    insert Kategoriler

---
+++ SqlBitirmeProjesi/insert_islemleri/insert_kategoriler.sql
@@ -0,0 +1,11 @@
+INSERT INTO Kategoriler (Ad) VALUES
+('Elektronik'),
+('Giyim'),
+('Ev & Yaþam'),
+('Kitap'),
+('Spor & Outdoor'),
+('Oyuncak'),
+('Kozmetik'),
+('Mutfak Gereçleri'),
+('Hobi & Sanat'),
+('Kýrtasiye');
