c921b0298e290ff9bd055c79fddd7e1aef03f6a5
Zeynep <138984822+zeyneptekkk@users.noreply.github.com>
2025-09-30 20:07:47 +0300

    delete islemleri

--- SqlBitirmeProjesi/Delete-Truncate_işlemleri/delete.sql
+++ SqlBitirmeProjesi/Delete-Truncate_işlemleri/delete.sql
@@ -1,9 +1,16 @@
-/*
-DELETE
-FROM TABLOADI
-WHERE <ÞARTLAR>
-*/
 
---where koþulu kullanýlmazsa tüm tablo silinir
 
-DELETE FROM CUSTOMERS
+select*from Kategoriler
+insert into Kategoriler(ad) values('spor')
+delete from Kategoriler where kategoriID=12
+
+select* from Musteriler 
+delete from Musteriler where musteriID=145
+
+select *from Saticilar
+delete from Saticilar where adres='Ýzmir' and saticiID between 1 and 3
+
+select*from SiparisDetaylari 
+delete from SiparisDetaylari where adet=4 and siparisID=43
+
+
