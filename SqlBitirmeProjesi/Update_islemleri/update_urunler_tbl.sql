6f24a6d6dc724ad40f19a671e96d6c38dae3f0cc
Zeynep <138984822+zeyneptekkk@users.noreply.github.com>
2025-09-30 22:08:34 +0300

    .

--- SqlBitirmeProjesi/Update_islemleri/update_urunler_tbl.sql
+++ SqlBitirmeProjesi/Update_islemleri/update_urunler_tbl.sql
@@ -4,11 +4,11 @@ update Urunler set ad='Kapiton' where urunID=222
 
 update Urunler set stock=45 where kategoriID=1
 
-update Urunler set kategoriID=3 where ad='Akýllý Telefon'
+update Urunler set kategoriID=3 where ad='AkÃ½llÃ½ Telefon'
 
-update Urunler set ad='Akýllý Telefon' where stock=70
+update Urunler set ad='AkÃ½llÃ½ Telefon' where stock=70
 
-update Urunler set ad='Smartwatch'  where ad='Gömlek'
+update Urunler set ad='Smartwatch'  where ad='GÃ¶mlek'
 
 update Urunler set ad='Mouse Pad' where fiyat<26
 
@@ -20,8 +20,33 @@ update Urunler set  ad='Hoodie' where saticiID between 3 and 5
 
 update Urunler set  stock=49 where saticiID=29 or saticiID=33
 
-update Urunler set ad='Akýllý Telefon' where ad like '%k'
+update Urunler set ad='AkÃ½llÃ½ Telefon' where ad like '%k'
 
 update Urunler set ad='Laptop' where stock in(45,50)
 
+
 update Urunler set fiyat=556 where saticiID in(47,45)
+
+CREATE PROCEDURE Sp_SiparisEkle
+    @urunID INT,
+    @adet INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Stok kontrolÃ¼ ve gÃ¼ncelleme
+    UPDATE Urunler
+    SET Stock = Stock - @adet
+    WHERE UrunID = @urunID
+      AND Stock >= @adet;
+
+    IF @@ROWCOUNT = 0
+    BEGIN
+        RAISERROR('Stok yetersiz, sipariÅŸ eklenemedi.', 16, 1);
+        RETURN;
+    END
+
+    -- SipariÅŸ ekleme
+    INSERT INTO OrderItems(UrunID, Quantity)
+    VALUES (@urunID, @adet);
+END
