b6815ef5e2173b6a30e9407b62ba02e453695d7f
Zeynep Tek <tekzeynep043@gmail.com>
2025-09-30 18:04:04 +0300

    update

---
+++ SqlBitirmeProjesi/Update_islemleri/update_musteriler_tbl.sql
@@ -0,0 +1,17 @@
+
+
+select * from Musteriler
+
+update Musteriler set sehir='Siirt' where musteriID=7
+
+update Musteriler set sehir='Batman' where musteriID=33
+
+update Musteriler set kayitTarihi='2025-09-29 22:59:13.5601914' where musteriID=1
+
+update Musteriler set ad='Ali' where eposta ='mert.yildiz24@mail.com'
+
+update Musteriler set soyad='Tek' where musteriID=62
+
+update Musteriler set eposta='merve_tek47@mail.com' where eposta='deniz.tek47@mail.com'
+
+
