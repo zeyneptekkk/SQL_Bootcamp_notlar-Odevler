588f3883caf265f3c70f7504049688918f8abbd4
Zeynep <138984822+zeyneptekkk@users.noreply.github.com>
2025-09-30 19:41:27 +0300

    Urunler tablosu update islemleri

---
+++ SqlBitirmeProjesi/Update_islemleri/update_urunler_tbl.sql
@@ -0,0 +1,27 @@
+select*from Urunler
+
+update Urunler set ad='Kapiton' where urunID=222
+
+update Urunler set stock=45 where kategoriID=1
+
+update Urunler set kategoriID=3 where ad='Akýllý Telefon'
+
+update Urunler set ad='Akýllý Telefon' where stock=70
+
+update Urunler set ad='Smartwatch'  where ad='Gömlek'
+
+update Urunler set ad='Mouse Pad' where fiyat<26
+
+update Urunler set stock=55 where fiyat between 150.00 and 200.00
+
+update Urunler set  ad='Dumbbell Seti' where fiyat between 50.00 and 70.00
+
+update Urunler set  ad='Hoodie' where saticiID between 3 and 5 
+
+update Urunler set  stock=49 where saticiID=29 or saticiID=33
+
+update Urunler set ad='Akýllý Telefon' where ad like '%k'
+
+update Urunler set ad='Laptop' where stock in(45,50)
+
+update Urunler set fiyat=556 where saticiID in(47,45)
