7c336e8ebfab4c4172629e9e82ffdc241dab7619
Zeynep <138984822+zeyneptekkk@users.noreply.github.com>
2025-09-30 18:20:40 +0300

    Saticilar tablosunun update işlemleri

---
+++ SqlBitirmeProjesi/Update_islemleri/update_saticilar_tbl.sql
@@ -0,0 +1,24 @@
+
+
+select * from Saticilar
+
+update Saticilar set ad='Kitapçý333' where saticiID=35
+
+update Saticilar set  adres='Siirt' where saticiID=3
+
+update Saticilar set  adres='Kurtalan' where saticiID=41
+
+update Saticilar set ad='MakyajMalzemeleri' where saticiID=44
+
+update Saticilar set adres='Batman' where saticiID=47 
+
+update Saticilar set adres='Þýrnak' where saticiID=50
+
+update Saticilar set  adres='Samasun' where saticiID=33
+
+update Saticilar set adres='Mardin' where saticiID=5
+
+update Saticilar set ad='EvAletleri' where saticiID=12
+
+update Saticilar set adres='Diyarbakýr' where saticiID=7 
+
