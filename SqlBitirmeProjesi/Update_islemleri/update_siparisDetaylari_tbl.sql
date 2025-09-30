ead58828da27abac22ebc4dfe4638ca2312398d3
Zeynep <138984822+zeyneptekkk@users.noreply.github.com>
2025-09-30 18:40:01 +0300

    SiparisDetaylari tablosu update islemleri

---
+++ SqlBitirmeProjesi/Update_islemleri/update_siparisDetaylari_tbl.sql
@@ -0,0 +1,20 @@
+select * from  SiparisDetaylari
+
+update SiparisDetaylari set urunID=222 where siparisID=12
+
+update SiparisDetaylari set adet=6 where detayID=18
+
+update SiparisDetaylari set adet=9 where urunID=209
+
+update SiparisDetaylari set  birimFiyat=223.55 where detayID=21
+
+update SiparisDetaylari set adet=10 where urunID=206
+
+update SiparisDetaylari set adet=2 where birimFiyat=810.10
+
+update SiparisDetaylari set birimFiyat=810.10 where birimFiyat=415.30
+
+update SiparisDetaylari set  birimFiyat=810.10 where adet=9,
+
+select * from SiparisDetaylari where adet=9
+
